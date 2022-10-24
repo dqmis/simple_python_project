a1da0be632581646348b80f30f4245cee237a5b0
Dominykas Šeputis <dom.seputis@gmail.com>
2022-10-24 18:16:47 +0300

    Update helpers.py

--- calculator/utils/helpers.py
+++ calculator/utils/helpers.py
@@ -15,7 +15,9 @@ def input_parser(fn: Callable) -> Callable:
     def convert_to_int(*args) -> Optional[int]:
         # TODO: Enable this function to work with multiple args and kwargs
         try:
-            return fn(args[0], int(args[1]))
+            parsed_args = [int(arg) for arg in args[1:]]
+            parsed_args.insert(0, args[0])
+            return fn(*parsed_args)
         except Exception:
             print(f"Cannot convert input of type {type(args[1])} to an int")
             return None
