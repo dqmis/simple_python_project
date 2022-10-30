4774ae7478eff825a375813b05ccb776787a9fd0
laimislau <laucius.laimis@yahoo.com>
2022-10-30 13:50:14 +0200

    timer is working. logger is not

--- calculator/utils/helpers.py
+++ calculator/utils/helpers.py
@@ -20,21 +20,22 @@ def timer(fn: Callable) -> Callable:
 def calculator_logger(fn: Callable) -> Callable:
     # TODO: Log function name and it's args and kwargs
     def logger(*args, **kwargs):
-        logging.info(f"{datetime.now()} test Runing a function {fn.__name__} w/ {args} and {kwargs}")
-        return fn()
+        t = datetime.now()        
+        logging.info(f"{t} Runing a function {fn.__name__} w/ {args} and {kwargs}")
+        return fn
     return logger
 
-
+"""
 def input_parser(fn: Callable) -> Callable:
     def convert_to_int(*args, **kwargs) -> Optional[int]:
         # TODO: Enable this function to work with multiple args and kwargs
         try:
             #return fn(args[0], int(args[1]))
-            return fn(*args, int(**kwargs))
+            return fn(*args, **kwargs)
         except Exception:
             print(f"Cannot convert input of type {type(*args)} to an int")
             return None
 
     return convert_to_int
 
-
+"""
